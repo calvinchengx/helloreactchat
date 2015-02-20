df61c32c20c7477b3e6295a5b2282ddbef9e5f9d
Calvin Cheng <linchuan.cheng@gmail.com>
2015-02-20 14:54:15 +0800

    Implement an example of using state in our react component

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -10,8 +10,20 @@ var JustForFun = React.createClass({
 });
 
 var HelloWorld = React.createClass({
+  getInitialState: function() {
+    return { counter: 0 };
+  },
+  increment: function() {
+    this.setState({ counter: this.state.counter + 1 });
+  },
   render: function() {
-    return (<div>Hello World! Greetings {this.props.name}</div>);
+    return (
+      <div>
+        <div>Hello World! Greetings {this.props.name}</div>
+        <div>{this.state.counter}</div>
+        <button onClick={this.increment}>Increment!</button>
+      </div>
+    );
   }
 });
 
