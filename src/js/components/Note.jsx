2319a392c14b558ca20aa747c0b77e746b3f07fe
Calvin Cheng <linchuan.cheng@gmail.com>
2015-02-20 22:53:21 +0800

    Demonstrate the use of Dispatcher, Actions and action binding to our React Component

--- src/js/components/Note.jsx
+++ src/js/components/Note.jsx
@@ -1,10 +1,51 @@
 'use strict';
 
-var React = require('react');
+// Dispatcher
+var Dispatcher = require('flux').Dispatcher;
+var assign = require('object-assign');
+var NoteAppDispatcher = assign(new Dispatcher(), {
+  handleViewAction: function(action) {
+    console.log('action', action);
+    var payload = {
+      source: 'VIEW_ACTION',
+      action: action
+    };
+    this.dispatch(payload);
+  }
+});
 
+// Action Constants is simply a way for us to list
+// all the possible action types related to user interactions
+// with Note
+var NoteConstants = {
+  VIEW_ITEM: 'VIEW_ITEM',
+  ADD_ITEM: 'ADD_ITEM'
+};
+
+// Actions that we can perform on a note
+var NoteActions = {
+  viewItem: function(index) {
+    NoteAppDispatcher.handleViewAction({
+      actionType: NoteConstants.VIEW_ITEM,
+      index: index 
+    });
+  },
+  addItem: function(item) {
+    NoteAppDispatcher.handleViewAction({
+      actionType: NoteConstants.ADD_ITEM,
+      item: item
+    });
+  }
+};
+
+// React Component
+var React = require('react');
 var Note = React.createClass({
+  handleClick: function() {
+    NoteActions.addItem('This is a new item');
+  },
   render: function() {
-    return <div>Note</div>  
+    return <div onClick={this.handleClick}>Note</div>  
   }
 });
 
